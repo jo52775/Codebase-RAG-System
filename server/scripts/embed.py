7c02e61ea37d3b4cbe774e7b38c04c39b6daddf9
Joshua Naraine <somal.naraine@gmail.com>
2026-09-14 11:29:46 -0400

    Changes to codebase embedding phase - new model setup to separate class and function chunks, optimized chunking strategy to include references to parent function and parent class, implements skeleton code for classes

--- server/scripts/embed.py
+++ server/scripts/embed.py
@@ -4,8 +4,8 @@ from langsmith import AuthenticationError
 from dotenv import load_dotenv
 from loguru import logger
 from sqlalchemy.orm import Session
-from database.models.chunk import Chunk
-from sqlalchemy import insert
+from database.models.chunk import FunctionModel, ClassModel
+from sqlalchemy import delete, insert
 from sqlalchemy.exc import SQLAlchemyError
 
 load_dotenv()
@@ -21,55 +21,120 @@ embed = OpenAIEmbeddings(
     api_key=api_key
 )
 
-def embed_codebase(db: Session, chunked_files):
-    total_rows_inserted = 0
+def clear_database(db: Session):
     try:
-        for filename, chunks in chunked_files.items():
-            if not chunks:
-                logger.info(f"Skipping file: {filename} \n")
-                continue
-            logger.info(f"Starting embedding process for {filename}. {len(chunks)} chunks found. \n")
+        db.execute(
+            delete(FunctionModel).where(FunctionModel.code_repository == repo_name)
+        )
+        db.execute(
+                delete(ClassModel).where(ClassModel.code_repository == repo_name)
+            )
+        db.commit()
+        logger.info("Database cleared successfully.")
+    except SQLAlchemyError as db_err:
+                    logger.error(f"Database insert staging failed for classes: {db_err}")
+                    raise db_err 
 
+
+def embed_classes(db: Session, classes: list[dict]):
+    total_rows_inserted = 0
+    valid_class_fqns = set()
+    try:
+        code_chunks = [class_chunk["raw_code_text"] for class_chunk in classes]
+        try:
+            if not classes:
+                return 0, set()
+            vector_embeddings = embed.embed_documents(code_chunks)
+            logger.info(f"Embedding successful for classes. {len(vector_embeddings)} vectors created. \n")
+        except Exception as e:
+            logger.error(f"OpenAI API generation failed for classes: {e}")
+            raise e
+
+        records = []
+        for idx, chunk in enumerate(classes):
+            record = {
+                "vector": vector_embeddings[idx],
+                "fqn": chunk["fqn"],
+                "raw_code_text": chunk["raw_code_text"],
+                "filename": chunk["filename"],
+                "skeleton_text": chunk["skeleton_text"],
+                "start_line": chunk["start_line"],
+                "end_line": chunk["end_line"],
+                "code_repository": chunk["code_repository"],
+            }
+            records.append(record)
+            valid_class_fqns.add(chunk["fqn"])
+
+        if records:
             try:
-                code_chunks = [chunk["text"] for chunk in chunks]
-                vector_embeddings = embed.embed_documents(code_chunks)
-                logger.info(f"Embedding successful for {filename}. {len(vector_embeddings)} vectors created. \n")
-            except Exception as e:
-                logger.error(f"OpenAI API generation failed for {filename}: {e}")
-                raise e
-
-            records = []
-            for idx, chunk in enumerate(chunks):
-                metadata = chunk["metadata"]
-                record = {
-                    "vector": vector_embeddings[idx],
-                    "raw_code_text": chunk["text"],
-                    "filename": metadata["filename"],
-                    "start_line": metadata["start_line"],
-                    "end_line": metadata["end_line"],
-                    "entity_name": metadata["entity_name"],
-                    "entity_type": metadata["entity_type"],
-                    "code_repository": repo_name,
-                }
-                records.append(record)
-            if records:
-                try:
-                    db.execute(insert(Chunk), records)
-                    total_rows_inserted += len(records)
-                    logger.info(f"Staged {len(records)} records for file: {filename} \n")
-                except SQLAlchemyError as db_err:
-                    logger.error(f"Database insert staging failed for {filename}: {db_err}")
-                    raise db_err
+                db.execute(insert(ClassModel), records)
+                total_rows_inserted += len(records)
+                logger.info(f"Staged {len(records)} class records \n")
+            except SQLAlchemyError as db_err:
+                logger.error(f"Database insert staging failed for classes: {db_err}")
+                raise db_err
 
         db.commit()
-        logger.info("Successfully stored all embeddings and metadata for codebase. \n")
-        return total_rows_inserted
-
+        logger.info("Successfully stored all class embeddings and metadata from the codebase. \n")
+        return total_rows_inserted, valid_class_fqns
+                
     except AuthenticationError:
         db.rollback() 
         logger.error("Authentication Failed: OpenAI API key is invalid or has expired.")
         raise
     except Exception as e:
         db.rollback() 
-        logger.error(f"Something went wrong with the codebase embedding: {e}")
+        logger.error(f"Something went wrong with embedding and/or saving class chunks: {e}")
+        raise e
+
+def embed_functions(db: Session, functions: list[dict], valid_class_fqns: set[str]):
+    total_rows_inserted = 0
+    try:
+        code_chunks = [function_chunk["raw_code_text"] for function_chunk in functions]
+        try:
+            vector_embeddings = embed.embed_documents(code_chunks)
+            logger.info(f"Embedding successful for functions. {len(vector_embeddings)} vectors created. \n")
+        except Exception as e:
+            logger.error(f"OpenAI API generation failed for functions: {e}")
+            raise e
+
+        records = []
+        for idx, chunk in enumerate(functions):
+            chunk_parent_class_fqn = chunk["parent_class_fqn"]
+            if chunk_parent_class_fqn not in valid_class_fqns:
+                chunk_parent_class_fqn = None
+
+            record = {
+                "vector": vector_embeddings[idx],
+                "raw_code_text": chunk["raw_code_text"],
+                "filename": chunk["filename"],
+                "entity_fqn": chunk["entity_fqn"],
+                "parent_fqn": chunk["parent_fqn"],
+                "parent_class_fqn": chunk_parent_class_fqn,
+                "start_line": chunk["start_line"],
+                "end_line": chunk["end_line"],
+                "code_repository": chunk["code_repository"],
+            }
+            records.append(record)
+
+        if records:
+            try:
+                db.execute(insert(FunctionModel), records)
+                total_rows_inserted += len(records)
+                logger.info(f"Staged {len(records)} function records \n")
+            except SQLAlchemyError as db_err:
+                logger.error(f"Database insert staging failed for functions: {db_err}")
+                raise db_err
+
+        db.commit()
+        logger.info("Successfully stored all function embeddings and metadata from the codebase. \n")
+        return total_rows_inserted
+                
+    except AuthenticationError:
+        db.rollback() 
+        logger.error("Authentication Failed: OpenAI API key is invalid or has expired.")
+        raise
+    except Exception as e:
+        db.rollback() 
+        logger.error(f"Something went wrong with embedding and/or saving function chunks: {e}")
         raise e
